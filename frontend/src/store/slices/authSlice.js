0ff59fe03df618f900e6c5efb9ada766396b6de0
Ahmed Ashraf <143915735+Ahm-edAshraf@users.noreply.github.com>
2024-11-30 23:05:17 +0800

    Update authSlice.js

--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://mern-todo-app-v42f.onrender.com/api';
 
 export const register = createAsyncThunk(
   'auth/register',
